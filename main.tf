c95d4c157989fbacfa797592b372bcdc1b9ca2d9
ThotaBlesson <blesson.aws1205@gmail.com>
2024-10-29 14:19:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ region = "ap-southeast-2"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-0f71013b2c8bd2c29"
+ami = "ami-084e237ffb23f8f97"
 instance_type = "t2.medium"
 key_name = "one"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+vpc_security_group_ids = ["sg-06f148abbd53d5dc1"]
 tags = {
 Name = var.instance_names[count.index]
 }
